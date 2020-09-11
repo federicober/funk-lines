d03b9903b431ff9163a52e3d9e6d1e45b33bb043
foberndorfer <foberndorfer@eulidia.com>
2020-09-12 01:50:59 +0200

    add pre commit hooks

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 """Sphinx configuration."""
 from datetime import datetime
 
 
 project = "Funk Lines"
 author = "Federico Oberndorfer"
-copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 autodoc_typehints = "description"
